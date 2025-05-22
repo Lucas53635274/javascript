90da71ddf826a07dd369152caade7447ecb3e5e9
chuparola2 <142441782+Lucas53635274@users.noreply.github.com>
2025-05-22 09:52:14 -0300

    Add files via upload

---
+++ exercicio 2/script2.js
@@ -0,0 +1,33 @@
+function somar(){
+    let num1 = Number(window.prompt("Digite aqui o primeiro número"));
+    let num2 = Number(window.prompt("Digite o segundo número"));
+
+    let resultado = num1 + num2;     
+
+    alert("O resultado é " + resultado + ".");
+}
+
+function subtrair(){
+    let num1 = Number(window.prompt("Digite aqui o primeiro número"));
+    let num2 = Number(window.prompt("Digite o segundo número"));
+
+    let resultado = num1 - num2;
+
+    alert("O resultado é " + resultado + ".");
+}
+function multiplicação(){
+    let num1 = Number(window.prompt("Digite aqui o primeiro número"));
+    let num2 = Number(window.prompt("Digite o segundo número"));
+
+    let resultado = num1 * num2;
+
+    alert("O resultado é " + resultado + ".");
+}
+function divisao(){
+    let num1 = Number(window.prompt("Digite aqui o primeiro número"));
+    let num2 = Number(window.prompt("Digite o segundo número"));
+
+    let resultado = num1 / num2;
+
+    alert("O resultado é " + resultado + ".");
+}
